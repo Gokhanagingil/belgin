a119da67e6b5b6b2cc0f9a8e5923c0a82082b902
Gokhanagingil <gokhan.agingil@gmail.com>
2026-09-01 23:15:22 +0300

    chore: refresh v4.3 offline cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kus-koyu-v4-2";
+const CACHE = "kus-koyu-v4-3";
 const BASE = self.registration.scope;
 const SHELL = [BASE, `${BASE}index.html`, `${BASE}manifest.webmanifest`, `${BASE}icon.svg`];
 
